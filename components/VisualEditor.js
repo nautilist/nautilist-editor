494abfb85ff7b3d05861fe9e1cbe8e76f1d60ea4
joeyklee <jsphknglee@gmail.com>
2019-02-13 14:54:49 -0500

    added sortable lists/ TODO: emit changes to json and yaml

--- components/VisualEditor.js
+++ components/VisualEditor.js
@@ -1,6 +1,8 @@
 var Component = require('choo/component')
 var html = require('choo/html')
 var css = require('sheetify')
+const Sortable = require('sortablejs');
+
 
 css`
 .small{
@@ -16,30 +18,61 @@ function createUrlList(_urlList){
 
   // recursion!
   console.log("running!")
-  return _urlList.map(feature => {
-    if(feature.hasOwnProperty('features')){
-      return html`
-      <fieldset class="ba b--dark-pink bw2">
-        <legend>List</legend>
-      ${createUrlList(feature.features)}
-      </fieldset>
-      `
-    } else {
-      return html`
-      <div class="w-100 flex flex-column ba bw1 mt2">
-      <div class="w-100 pl2 pr2 bg-light-green truncate">
-        <small class="ma0 small">${feature.url}</small>
-      </div>
-      <div class="w-100 flex flex-row">
-        <div class="w-40 pa2">
-          <p class="f6 b ma0"> <a class="link black" href="${feature.url}" target="_blank">${feature.name} </a></p>
+  return html`
+  <section>
+  ${
+    _urlList.map(feature => {
+      if(feature.hasOwnProperty('features')){
+        return html`
+        <fieldset class="ba b--dark-pink bw2">
+          <legend>List</legend>
+        ${createSortable(createUrlList(feature.features)) }
+        </fieldset>
+        `
+      } else {
+        return html`
+        <div class="w-100 flex flex-column ba bw1 mt2">
+          <!-- url -->
+          <div class="w-100 pl2 pr2 bg-light-green truncate">
+            <small class="ma0 small">${feature.url}</small>
+          </div>
+          <!-- link details -->
+          <div class="w-100 flex flex-row">
+            <div class="w-40 pa2">
+              <p class="f6 b ma0"> <a class="link black" href="${feature.url}" target="_blank">${feature.name} </a></p>
+            </div>
+            <div class="w-60 pl2 pa2">
+              <p class="f6 ma0">${feature.description}</p>
+            </div>
+          </div>
         </div>
-        <p class="w-60 f6 pl2 ma0 pa2">${feature.description}</p>
-      </div>
-    </div>
-      `
-    }
-  })
+        `
+      }
+    })
+  }
+  </section>
+  `
+
+}
+
+function createSortable(_newList){
+  let newList, sortableList;
+    sortableList = Sortable.create(_newList, {
+      onEnd: function(evt){
+        console.log("sortable", evt.newIndex);
+        // console.log("🌮🌮🌮",evt.clone.dataset.parentid, evt.clone.dataset.parentdb);
+        // const payload = {
+        //   parentBranchId: evt.clone.dataset.parentid,
+        //   parentCollection: evt.clone.dataset.parentdb,
+        //   recipeId: evt.clone.dataset.id,
+        //   newRecipePosition: evt.newIndex
+        // }
+        // emit(state.events.projects_reorderRecipes, payload)
+        // emit("db:selectedFeature:reorder", evt.clone.dataset.parentid, evt.clone.dataset.parentdb,  evt.clone.dataset.featureid, evt.newIndex)
+      }
+    });
+
+    return sortableList.el;
 }
 
 
@@ -56,6 +89,8 @@ class VisualEditor extends Component {
     const {json} = this.state.workspace;
     if(!json){ return html`<div class="flex flex-row w-100 justify-center mt4">No lists yet! 🏝</div>`}
 
+    const mySortableList = createSortable( createUrlList(json.features) )
+
     return html`
       <div class="w-100 h-100 pl2 pr2 overflow-y-scroll">
         <header class="w-100 flex flex-column pl2 pr2">
@@ -63,7 +98,7 @@ class VisualEditor extends Component {
           <p class="f2 lh-copy mt0 mb2">${json.description ||  "Add an awesome list description"}</p>
         </header>
         <section class="w-100">
-          ${createUrlList(json.features)}
+          ${mySortableList}
         </section>
       </div>
     `
