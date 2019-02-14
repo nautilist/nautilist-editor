aa3379dce82c0182cb79ff020f4324f3faa71d72
joeyklee <jsphknglee@gmail.com>
2019-02-14 18:02:03 -0500

    create component from AboutModal

---
+++ components/AboutModal.js
@@ -0,0 +1,120 @@
+var Component = require('choo/component')
+var html = require('choo/html')
+
+class AboutModal extends Component {
+  constructor (id, state, emit) {
+    super(id)
+    this.local = state.components[id] = {}
+    this.state = state;
+    this.emit = emit;
+    this.open = this.open.bind(this);
+    this.displayed = 'dn';
+    this.rerender = this.rerender.bind(this)
+  }
+
+  open(){
+    return e => {
+      console.log("opening!")
+      this.displayed = 'flex';
+      this.rerender();
+    }
+    
+  }
+  
+  close(){
+    return e => {
+      console.log("closing!")
+      this.displayed = 'dn';
+      this.rerender();
+    }
+    
+  }
+
+  createElement () {
+    return html`
+    <div id="helpModal" class="w-100 h-100 flex-column justify-center items-center ${this.displayed} fixed top-0 left-0 max-z pa4" style="background:rgba(255, 65, 180,0.7)">
+      <div class="w-100 h-auto mw7 pa4 ba dropshadow br2 bg-white overflow-y-scroll">
+        <header class="flex flex-row items-center justify-between">
+          <h2>About</h2>
+          <button class="bn bg-purple white bw2 pa h2 w2" onclick="${this.close()}">⨉</button>
+        </header>
+        <section class="w-100 mt2 mb2">
+          <p>Hello and welcome to the Nautlist Editor!</p>
+          <p>It's a lightweight editor for putting together flexible lists of resources using YAML, a structured, human-computer readable data structure. <a class="link black underline" href="https://nautilist.github.io/" target="_blank">Learn more.</a></p>
+          <p>To get get started - copy/paste this list boilerplate 👇 and press the "run" button:</p>
+        </section>
+        <!-- simple single list -->
+        <section>
+        <h3>Simple list</h3>
+${singleListExample()}
+        </section>
+        <!-- simple multi list -->
+        <section>
+        <h3>Simple list of lists</h3>
+${multiListExample()}
+        </section>
+        <button class="w-100 h3 bn bg-purple white pa2 mt3 mb3" onclick=${this.close()}>close</button>
+      </div>
+    </div>
+    `
+  }
+
+  update () {
+    return false
+  }
+}
+
+module.exports = AboutModal
+
+function multiListExample(){
+  return html`
+<pre class="mt2 bg-light-gray pa2 f7">
+<code>
+type: list
+name: Nautilist Simple Boilerplate
+description: A boilerplate list of lists for nautilist
+features:
+  - type: list
+    name: My Special List
+    description: A list 1 description
+    features:
+      - url: www.itp.nyu.edu
+        name: ITP/IMA
+        description: Website to NYU's ITP/IMA program
+      - url: www.nautilist.github.io/
+        name: Nautilist homepage
+        description: Nautilist is a tool for ...
+  - type: list
+    name: My Other Special List
+    description: A list 2 description
+    features:
+      - url: www.itp.nyu.edu
+        name: ITP/IMA for list 2
+        description: Website to NYU's ITP/IMA program
+      - url: www.nautilist.github.io/
+        name: Nautilist homepage for list 2
+        description: Nautilist is a tool for ...
+  </code>
+</pre>
+  `
+}
+
+function singleListExample(){
+  return html`
+<pre class="mt2 bg-light-gray pa2 f7">
+<code>
+type: list
+name: Nautilist Simple Boilerplate
+description: A boilerplate list for nautilist
+features:
+  - url: www.itp.nyu.edu
+    name: ITP/IMA
+    description: Website to NYU's ITP/IMA program
+  - url: www.nautilist.github.io/
+    name: Nautilist homepage
+    description: Nautilist is a tool for ...
+</code>
+</pre>
+  
+  `
+}
--- views/main.js
+++ views/main.js
@@ -3,20 +3,19 @@ const css = require('sheetify')
 var FileSaver = require('file-saver');
 const slugify = require('slugify')
 
+const TITLE = 'Nautilist Web Editor'
+
+const CodeEditor = require("../components/CodeEditor");
+const VisualEditor = require("../components/VisualEditor");
+const AboutModal = require("../components/AboutModal");
+
 css`
 html{
   width:100%;
   height:100%;
   padding: 1em;
 }
-`
 
-const TITLE = 'Nautilist Web Editor'
-
-const CodeEditor = require("../components/CodeEditor");
-const VisualEditor = require("../components/VisualEditor");
-
-css`
 .dropshadow{
   box-shadow:2px 2px black;
 }
@@ -35,6 +34,8 @@ module.exports = view
 function view (state, emit) {
   const codeEditor = new CodeEditor(state, emit);
   const visualEditor = new VisualEditor(state, emit);
+  const aboutModal  = new AboutModal("AboutModal", state, emit)
+
   if (state.title !== TITLE) emit(state.events.DOMTITLECHANGE, TITLE)
 
   function updateEditorView(){
@@ -57,80 +58,6 @@ function view (state, emit) {
     // FileSaver.saveAs(blob, fileName);
   }
 
-  function HelpModal(){
-    return html`
-      <div id="helpModal" class="w-100 h-100 flex-column justify-center items-center dn fixed top-0 left-0 max-z pa4" style="background:rgba(255, 65, 180,0.7)">
-        <div class="w-100 h-auto mw7 pa4 ba dropshadow br2 bg-white overflow-y-scroll">
-          <header class="flex flex-row items-center justify-between">
-            <h2>About</h2>
-            <button class="bn bg-purple white bw2 pa h2 w2" onclick="${closeHelp}">⨉</button>
-          </header>
-          <p>Hello and welcome to the Nautlist Editor!</p>
-          <p>It's a lightweight editor for putting together flexible lists of resources using YAML, a structured, human-computer readable data structure. <a class="link black underline" href="https://nautilist.github.io/" target="_blank">Learn more.</a></p>
-          <p>To get get started - copy/paste this list boilerplate 👇 and press the "run" button:</p>
-          
-          <h3>Simple list</h3>
-          <pre class="mt2 bg-light-gray pa2 f7">
-<code>
-type: list
-name: Nautilist Simple Boilerplate
-description: A boilerplate list for nautilist
-features:
-  - url: www.itp.nyu.edu
-    name: ITP/IMA
-    description: Website to NYU's ITP/IMA program
-  - url: www.nautilist.github.io/
-    name: Nautilist homepage
-    description: Nautilist is a tool for ...
-          </code>
-          </pre>
-
-          <h3>Simple list of lists</h3>
-          <pre class="mt2 bg-light-gray pa2 f7">
-<code>
-type: list
-name: Nautilist Simple Boilerplate
-description: A boilerplate list of lists for nautilist
-features:
-  - type: list
-    name: My Special List
-    description: A list 1 description
-    features:
-      - url: www.itp.nyu.edu
-        name: ITP/IMA
-        description: Website to NYU's ITP/IMA program
-      - url: www.nautilist.github.io/
-        name: Nautilist homepage
-        description: Nautilist is a tool for ...
-  - type: list
-    name: My Other Special List
-    description: A list 2 description
-    features:
-      - url: www.itp.nyu.edu
-        name: ITP/IMA for list 2
-        description: Website to NYU's ITP/IMA program
-      - url: www.nautilist.github.io/
-        name: Nautilist homepage for list 2
-        description: Nautilist is a tool for ...
-          </code>
-          </pre>
-
-          <button class="w-100 h3 bn bg-purple white pa2 mt3 mb3" onclick=${closeHelp}>close</button>
-        </div>
-      </div>
-    `
-  }
-
-  function closeHelp(){
-      document.querySelector('#helpModal').classList.toggle('flex')
-  }
-
-
-  function openHelp(e){
-      // alert("copy and paste into the editor to start:\n\n\ntype: list\nname:my title\ndescription: list description\nfeatures:\n- url:")
-    document.querySelector('#helpModal').classList.toggle('flex')
-  }
-
   function openShare(e){
     alert("TODO: share ")
   }
@@ -145,7 +72,7 @@ features:
       <header class="w-100 flex flex-row justify-between items-center pa2">
         <div>
           <button class="ba br-pill dropshadow ba b--dark-pink bg-white dark-pink bw1 pa2 mr2">🌈 Nautilist Editor ✨</button>
-          <button class="ba ba b--white bg-white navy bw1 pa2 mr2" onclick="${openHelp}"> About </button>
+          <button class="ba ba b--white bg-white navy bw1 pa2 mr2" onclick="${aboutModal.open()}"> About </button>
           <button class="ba ba b--white bg-white navy bw1 pa2 mr2" onclick="${openShare}"> Share </button>
           <button class="ba ba b--white bg-white navy bw1 pa2 mr2" onclick="${openSearch}"> 🔎 Search </button>
         </div>
@@ -168,27 +95,8 @@ features:
         </div>
       </section>
       </main>
-      ${HelpModal()}
+      ${aboutModal.render()}
     </body>
   `
 
 }
-
-// 
-/**
-<header class="w-100">
-        <div class="w-100 h-100 flex flex-row justify-between items-center pa2">
-        <div>
-        <button class="ba br-pill dropshadow ba b--dark-pink bg-white dark-pink bw1 pa2 mr2">🌈 Nautilist Editor ✨</button>
-        <button class="ba ba b--white bg-white navy bw1 pa2 mr2" onclick="${openHelp}"> About </button>
-        <button class="ba ba b--white bg-white navy bw1 pa2 mr2" onclick="${openShare}"> Share </button>
-        <button class="ba ba b--white bg-white navy bw1 pa2 mr2" onclick="${openSearch}"> 🔎 Search </button>
-        </div>
-        <div>
-          <button class="ba dropshadow ba b--white bg-yellow navy bw1 pa2 mr2" onclick="${updateEditorView}">▶ Run</button>
-          <button class="ba dropshadow ba b--white bg-navy dark-pink bw1 pa2 mr2" onclick="${saveYaml}">Save</button>
-          <button class="ba dropshadow ba b--white bg-white purple bw1 pa2" onclick="${openFile}">Open</button>
-        </div>
-        </div>
-      </header>
- */
