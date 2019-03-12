029a49338af257b402e0c1c4f28f49f0b5e5ce91
joeyklee <jsphknglee@gmail.com>
2019-03-12 11:40:38 -0400

    set feathers to heroku link

--- helpers/feathersClient.js
+++ helpers/feathersClient.js
@@ -4,7 +4,7 @@ const auth = require('@feathersjs/authentication-client');
 const config = require('../config.js');
 
 // Connect to a different URL
-const restClientUrl = 'https://localhost:3030' || config.NAUTILISTAPI
+const restClientUrl =  config.NAUTILISTAPI || 'https://localhost:3030'
 console.log(restClientUrl)
 const restClient = rest(restClientUrl)
 const feathersClient = feathers().configure(restClient.fetch(window.fetch));
