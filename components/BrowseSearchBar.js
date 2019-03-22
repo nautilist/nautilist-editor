36e310fef78a760fdb5c9b88464a25995c35212b
joeyklee <jsphknglee@gmail.com>
2019-03-22 00:00:53 -0400

    added browse search bar component

---
+++ components/BrowseSearchBar.js
@@ -0,0 +1,22 @@
+var Component = require('choo/component')
+var html = require('choo/html')
+
+class BrowseSearchBar extends Component {
+  constructor (id, state, emit) {
+    super(id)
+    this.local = state.components[id] = {}
+  }
+
+  createElement () {
+    return html`
+      <div>
+      </div>
+    `
+  }
+
+  update () {
+    return true
+  }
+}
+
+module.exports = BrowseSearchBar
