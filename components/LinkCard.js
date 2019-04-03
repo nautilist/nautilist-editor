16c5f230894ec47323dbc6b2b00102cba054974d
joeyklee <jsphknglee@gmail.com>
2019-04-03 12:26:27 -0400

    added button placeholder for saving link or adding to list

--- components/LinkCard.js
+++ components/LinkCard.js
@@ -15,19 +15,29 @@ module.exports = function(link){
         }
       }
 
+      function saveLink(e){
+          alert('save link modal')
+      }
+
 
     const {name, description, _id, selectedColor, colors, ownerDetails} = link;
 
     return html`
     <a class="fl w-100 w-25-l w-third-m h5 link black mb4" href="/link/${_id}">
-    <div class="h-100 shadow-5 bg-near-white ma2" data-type="links" data-id=${_id} onclick=${handleRedirect}>
-      <header class="w-100 h2  br--top" style="background-color:${colors[selectedColor]};"></header>
+    <div class="h-100 dropshadow bg-near-white ma2 flex flex-column justify-between" data-type="links" data-id=${_id} onclick=${handleRedirect}>
+        <div class="flex flex-column w-100">
+            <button class="w-100 dn f7 bn bg-navy dark-pink">save for later</button>
+            <button class="bn f7 w-100 h2 br--top" style="background-color:${colors[selectedColor]};"></button>
+        </div>
       <div class="hide-child">
         <section class="pa2">
           <h3 class="ma0">${name}</h3>
           <small class="ma0">by <a class="link black underline" href="/users/${ownerDetails.username}">${checkOwner(link)}</a></small>
         </section>
         <p class="ma0 pa2 child f7 overflow-y-scroll" style="max-height:6rem">${description}</p>
+      </div>
+      <div class="w-100 pa2 flex flex-row justify-end">
+        <button class="h2 w2 br-100 f7 bn bg-moon-gray white grow" onclick=${saveLink}>+</button>
       </div>
     </div>
     </a>
