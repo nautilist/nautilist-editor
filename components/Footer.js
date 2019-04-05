8182bd783af749295e7c4738773809d76350ffe1
joeyklee <jsphknglee@gmail.com>
2019-04-05 03:51:40 -0400

    updated footer

--- components/Footer.js
+++ components/Footer.js
@@ -4,17 +4,16 @@ var html = require('choo/html')
 function Footer(){
   return html`
     <footer class="w-100 bg-navy hot-pink pa4 flex flex-column flex-row-ns" style="min-height:300px">
-      <ul class="list pl0 w-third">
+      <ul class="list pl0 w-third-ns w-100">
         <li>Imprint</li>
         <li>License</li>
-      </ul>
-      <ul class="list pl0 w-third">
-      <li>Contact</li>
-      <li>Contribute</li>
+        <li><a class="link hot-pink" href="https://github.com/nautilist" target="_blank">Contribute</a></li>
+        <li><a class="link hot-pink" href="https://github.com/nautilist/nautilist-editor" target="_blank">Issues/Reporting</a></li>
       </ul>
 
-      <ul class="list pl0 w-third">
+      <ul class="list pl0 w-two-thirds-ns w-100">
         <li>© 2019 NAUTILIST IS AN OPEN EDUCATIONAL INITIATIVE BY ITP.</li>
+        <li>Contact: hello.nautilist at gmail dot com</li>
       </ul>
       
     </footer>
