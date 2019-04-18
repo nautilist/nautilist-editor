913a4fcfd3ff46d9f1872950da46cbf6212a17c6
joeyklee <jsphknglee@gmail.com>
2019-04-17 23:51:31 -0400

    fixed double verification email sending

--- views/verify.js
+++ views/verify.js
@@ -4,32 +4,33 @@ module.exports = view
 
 function onClose(state, emit){
   return e => {
+    e.preventDefault();
     emit('pushState', '/login')
   }
 }
 
-// function submitSendReset(state, emit){
-//   return e => {
-//     e.preventDefault();
-//     const formData = new FormData(e.currentTarget);
-//     emit(state.events.user_sendResetPassword, formData);
-//   }
-// }
-
-// function submitReset(state, emit){
-//   return e => {
-//     e.preventDefault();
-//     const formData = new FormData(e.currentTarget);
-//     emit(state.events.user_resetPassword, formData);
-//   }
-
-// }
+function triggerVerify(state, emit){
+  if(state.query.hasOwnProperty('token')){
+    console.log("yes!!")
+    const token = state.query.token
+    var obj = {
+      action: 'verifySignupLong',
+      value: token
+    }
+    state.api.authmanagement.create(obj)
+    .then(result => {
+      console.log('user verified!')
+      // alert('Sending your account verification - please check your inbox :)')
+    }).catch(err => {
+      return err;
+    })
+  }
+}
 
 function showInput(state, emit){
   if(state.query.hasOwnProperty('token')){
-    emit(state.events.user_sendVerificationToken);
-
-    return html`
+    console.log("yes!!")
+       return html`
     <div class="mw6 w-100 flex flex-column items-center">
         <h2>Your account was verified!</h2>
         <h3>Go to <a class="link black underline" href="/login">nautilists.com/login</a> to get started!</h3>
@@ -41,21 +42,22 @@ function showInput(state, emit){
       <h2>An email was sent to verify your account</h2>
     </div>
     `
-  }
+  } 
+ 
 }
 
 
 
 function view (state, emit) {
   return html`
-    <body class="code lh-copy w-100 h-100">
+    <body class="code lh-copy w-100 h-100" onload=${() => triggerVerify(state, emit)}>
       <div class="w-100 flex flex-row items-center justify-between">
         <a class="link black w3 ml2" href="/"><img src="/assets/logo-wow.png"></a>
         <button class="bn moon-gray bw2 pa2 h3 w3 f3 pointer" onclick="${onClose(state, emit)}">╳</button>
       </div>
       <main class="w-100 h-100">
           <div class="w-100 flex flex-column justify-center items-center">
-            ${showInput(state, emit)}
+          ${showInput(state, emit)}
             <small class="dn">Don't have an account? <a class="link black underline" href="/signup">Sign up!</a></small>
           </div>
           
